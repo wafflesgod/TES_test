f8fdadac6bdcab80dbc28942726fc917e857f552
wafflesgod <huimingchang09@gmail.com>
2025-11-12 09:31:56 +0800

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,28 @@
+import streamlit as st
+import clips
+import logging
 
-s = "hello"
-print(s)
+#Setup working environment
+logging.basicConfig(level=15, format='%(message)s')
+
+env = clips.Environment()
+router = clips.LoggingRouter()
+env.add_router(router)
+
+#input
+name = st.text_input("Enter your name:")
+
+#knowledge base
+env.build('(deftemplate result (slot name))')
+#add facts to working memory
+env.assert_string(f'(result(name "{name}"))')
+#inference
+env.run()
+
+#output
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result':
+        results.append(fact['name']) #why assert
+
+st.write(results[0], "better output")
